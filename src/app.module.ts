8651d9b614273af138cf29ea85150ef68ad665b9
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2026-01-21 07:51:25 +0300

    chore(database): add Prisma schema and database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExploreModule } from './explore/explore.module';
 import { IngestionModule } from './ingestion/ingestion.module';
+import { DatabaseModule } from './shared/database/database.module';
 import { SharedModule } from './shared/shared.module';
 import { StudioModule } from './studio/studio.module';
 
 @Module({
-  imports: [StudioModule, ExploreModule, IngestionModule, SharedModule],
+  imports: [
+    StudioModule,
+    ExploreModule,
+    IngestionModule,
+    SharedModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
