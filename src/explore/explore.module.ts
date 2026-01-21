6b4df6be7221be6f4fa380f605688c38c5c6df95
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2026-01-21 07:40:27 +0300

    chore: add studio and explore feature module skeletons

--- src/explore/explore.module.ts
+++ src/explore/explore.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BrowseModule } from './browse/browse.module';
+import { SearchModule } from './search/search.module';
 
-@Module({})
+@Module({
+  imports: [SearchModule, BrowseModule],
+})
 export class ExploreModule {}
--- src/studio/studio.module.ts
+++ src/studio/studio.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './categories/categories.module';
+import { EpisodesModule } from './episodes/episodes.module';
+import { ProgramsModule } from './programs/programs.module';
 
-@Module({})
+@Module({
+  imports: [ProgramsModule, EpisodesModule, CategoriesModule],
+})
 export class StudioModule {}
